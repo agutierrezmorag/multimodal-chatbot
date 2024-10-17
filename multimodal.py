e98528e84e3fe35df70c2463503c97f6fd50bd06
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2024-10-17 12:01:20 -0300

    attempt to fix chat_model not defined error in streamlit share

--- multimodal.py
+++ multimodal.py
@@ -41,6 +41,7 @@ if __name__ == "__main__":
     col1, col2 = st.columns(2)
     image_data = None
     api_key = None
+    chat_model = None
 
     with st.sidebar:
         col1, col2 = st.columns(2)
