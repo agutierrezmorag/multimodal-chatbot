4a49cf761ff32526d689b44c0972650ab511c45c
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2024-10-18 08:57:46 -0300

    Refactor df_page.py to add chatbot functionality and improve code structure

--- app_pages/df_page.py
+++ app_pages/df_page.py
@@ -1,4 +1,39 @@
+import pandas as pd
 import streamlit as st
+from langchain.callbacks.tracers import LangChainTracer
+from langchain_experimental.agents import create_pandas_dataframe_agent
+from langchain_openai import ChatOpenAI
+
+
+@st.cache_resource
+def get_agent(df):
+    llm = ChatOpenAI(model="gpt-4o-mini")
+    agent = create_pandas_dataframe_agent(
+        llm,
+        df,
+        agent_type="openai-tools",
+        verbose=True,
+        allow_dangerous_code=True,
+    )
+    return agent
+
 
 if __name__ == "__page__":
-    st.write("Hello, world!")
+    if "agent_response" not in st.session_state:
+        st.session_state.agent_response = None
+
+    tracer = LangChainTracer(project_name="DF Q&A")
+
+    df = pd.read_csv("SampleSuperstore.csv")
+    st.dataframe(df)
+    agent = get_agent(df)
+
+    if question := st.chat_input(placeholder="Escribe tu pregunta..."):
+        st.session_state.agent_response = agent.invoke(
+            {"input": question},
+            config={"callbacks": [tracer]},
+        )["output"]
+
+    if st.session_state.agent_response:
+        with st.chat_message("assistant"):
+            st.write(st.session_state.agent_response)
